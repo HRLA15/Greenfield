f25618356de5f0847092ae9b9895686b3d470504
Jay Hongvarivatana <jayyhong@gmail.com>
2017-06-22 15:25:02 -0700

    Uncommented out database config.js

--- Database/config.js
+++ Database/config.js
@@ -1,22 +1,22 @@
-// const Sequelize = require('sequelize')
-// require('dotenv').config();
+const Sequelize = require('sequelize')
+require('dotenv').config();
 
-// const db = new Sequelize("postgres://hnsciryi:KxVLjMQb9wglCm35-5LybTjwOHhwxCoZ@pellefant.db.elephantsql.com:5432/hnsciryi", {
-//   pool: {
-//     min: 0,
-//     max: 3,
-//     idle: 10000
-//   }
-// });
+const db = new Sequelize("postgres://hnsciryi:KxVLjMQb9wglCm35-5LybTjwOHhwxCoZ@pellefant.db.elephantsql.com:5432/hnsciryi", {
+  pool: {
+    min: 0,
+    max: 3,
+    idle: 10000
+  }
+});
 
-// db.authenticate()
-//   .then(() => {
-//     console.log('Successfully connected to the database')
-//   })
-//   .catch((err) => {
-//     console.log('Error connecting: ', err)
-//   })
+db.authenticate()
+  .then(() => {
+    console.log('Successfully connected to the database')
+  })
+  .catch((err) => {
+    console.log('Error connecting: ', err)
+  })
 
 
 
-// module.exports = db;
+module.exports = db;
