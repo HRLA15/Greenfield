12aec257373f1f1801791106dfc328e0c424f2f3
Jonathan <jyi1991@gmail.com>
2017-06-23 15:13:21 -0700

    made material-ui available everywhere

--- public/routes.js
+++ public/routes.js
@@ -9,6 +9,8 @@ import Create from './src/createTripPageComponent/Create';
 import Event from './src/tripSummaryComponent/TripSummary'
 import Landing from './src/LandingPage'
 import Navbar from './src/navbarComponent/Navbar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 const auth = new Auth();
 
@@ -19,8 +21,11 @@ const handleAuthentication = (nextState, replace) => {
 }
 
 export const makeMainRoutes = () => {
+  injectTapEventPlugin()
+  
   return (
       <BrowserRouter history={history}>
+        <MuiThemeProvider>
         <div>
           <Route path="/" render={(props) => <Navbar auth={auth} {...props} />} />
           <Route exact={true} path="/" component={Landing}/>
@@ -33,6 +38,7 @@ export const makeMainRoutes = () => {
           <Route path="/event" render={(props) => <Event auth={auth} {...props}/>}/>
        
         </div>
+         </MuiThemeProvider>
       </BrowserRouter>
   );
 }
