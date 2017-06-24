0bbcb334435075f1fc053ec8dfc827c1f2cbeae7
Jay Hongvarivatana <jayyhong@gmail.com>
2017-06-23 19:54:57 -0700

    "Edited create page, added autocomplete"

--- public/src/userHomePageComponent/UserHome.js
+++ public/src/userHomePageComponent/UserHome.js
@@ -58,6 +58,7 @@ class UserHome extends Component {
         <div syle={{flexGrow: 1, flexShrink: 1}}>
         <UserSideBar />
         </div>
+        <CreateTripButton handleCreateTripButtonClick={this.handleCreateTripButtonClick}/>
       </div>
     )
   }
