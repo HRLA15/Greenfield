6f3a3425c5b3e99b1ebbf0d3345d39ae7a83d018
Jay Hongvarivatana <jayyhong@gmail.com>
2017-06-25 15:19:36 -0700

    "Updated settings page"

--- public/src/settingPageComponent/UserProfilePic.js
+++ public/src/settingPageComponent/UserProfilePic.js
@@ -21,15 +21,15 @@ class UserProfilePic extends Component {
     
     let previewPic = null
 
-    if(this.props.userInfo.profilePic.length > 0 && this.state.accepted.length < 1) {
-      previewPic = this.props.userInfo.profilePic
+    if(this.props.userInfo.url.length > 0 && this.state.accepted.length < 1) {
+      previewPic = this.props.userInfo.url
     }
 
     if(this.state.accepted.length > 0) {
       previewPic = this.state.accepted[0].preview
     }
 
-    if(this.props.editPic || this.props.userInfo.profilePic.length < 1) {
+    if(this.props.editPic || this.props.userInfo.url.length < 1) {
       pic = (
 
       <div>
@@ -40,8 +40,9 @@ class UserProfilePic extends Component {
           onDrop={this.onDrop.bind(this)}
           style={{border: "solid 3px white", backgroundColor: "white", borderRadius: 10, height: 200 + "px", width: 200 + "px"}}
         >
-          <img src={previewPic} style ={{marginLeft: 15 + "px", marginTop: 15 + "px", backgroundColor: "#f2f2f2", borderRadius: 100 + "%"}} width="150" height="150"/>
+          <img src={previewPic} style ={{marginLeft: 15 + "px", marginTop: 15 + "px", backgroundSize: "cover", backgroundColor: "#f2f2f2", borderRadius: 100 + "%"}} width="150" height="150"/>
         </Dropzone>
+        <p style={{marginLeft: 25 + "px"}}>Drag and drop image</p>
 
         <FlatButton label="SAVE" primary={true} onClick={() => {
           this.props.handleSavePicClick(this.state.accepted)
@@ -59,7 +60,7 @@ class UserProfilePic extends Component {
     } else {
       pic = (
         <div>
-        <img src={this.props.userInfo.profilePic} alt="Upload Your Picture.."/>
+        <img src={this.props.userInfo.url} alt="Upload Your Picture.."/>
         <button onClick={this.props.handleEditPicClick}>Edit</button>
         </div>
       )
